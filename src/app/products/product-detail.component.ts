ea71ec185de736945a530588d91450976fa2f866
shirishgatika2003 <shirishgatika2003@users.noreply.github.com>
2018-10-06 12:35:36 -0500

    Initial commit

---
+++ src/app/products/product-detail.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'pm-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css']
+})
+export class ProductDetailComponent implements OnInit {
+  pageTitle : string = 'Product Details';
+  product: IProduct[] = [];
+  constructor(private route: ActivatedRoute,  private router: Router) { }
+
+  ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle+= `${id}`;
+    this.product = 
+      {
+        "productId": id,
+        "productName": "Garden Cart",
+        "productCode": "GDN-0023",
+        "releaseDate": "March 18, 2016",
+        "description": "15 gallon capacity rolling garden cart",
+        "price": 32.99,
+        "starRating": 4.2,
+        "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+      };
+    
+  }
+
+  onBack():void {
+    this.router.navigate(['/products']);
+  }
+
+}
---
+++ src/app/products/product.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { IProduct } from "./product";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import {catchError, tap } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ProductService {
+    private productUrl = 'api/products/products.json';
+    constructor(private http: HttpClient){ }
+    getProducts(): Observable<IProduct[]> {
+        return this.http.get<IProduct[]>(this.productUrl)
+            .pipe(
+                tap(data => console.log( JSON.stringify(data))),
+                catchError(this.handleError)
+            );
+    }
+
+    private handleError(err: HttpErrorResponse){
+        let errorMessage = '';
+        if(err.error instanceof ErrorEvent){
+            errorMessage = `An Error ${err.error.message}`;
+        } else {
+            errorMessage = `Server error ${err.status} and message ${err.message}`;
+        }
+        return throwError(errorMessage);
+    }
+
+}
